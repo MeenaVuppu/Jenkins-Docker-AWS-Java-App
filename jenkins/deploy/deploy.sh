c554473606e91a2346f5392b8177c3482e0c0aa4
Meena Vuppu <vuppumeena@gmail.com>
2023-03-27 12:10:12 +0530

    env var change

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,4 +6,4 @@ echo $PASSWORD >> /tmp/.auth
 
 scp -i /opt/MyPracticeInstanceKey /tmp/.auth ubuntu@18.205.119.48:/tmp/.auth
 scp -i /opt/MyPracticeInstanceKey ./jenkins/deploy/publish ubuntu@18.205.119.48:/tmp/publish
-ssh -i /opt/MyPracticeInstanceKey ubuntu@18.205.119.48  "/tmp/publish"
+ssh -i /opt/MyPracticeInstanceKey ubuntu@18.205.119.48 "/tmp/publish"
