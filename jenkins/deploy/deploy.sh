4d27e131ce643a09dbb0bf58b15fd51ed476b4f7
Meena Vuppu <vuppumeena@gmail.com>
2023-03-27 09:44:37 +0530

    modified key path

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo pratice > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASSWORD >> /tmp/.auth
 
-scp -i /home/meena/MyPracticeInstanceKey /tmp/.auth ubuntu@18.205.119.48:/tmp/.auth
-scp -i /home/meena/MyPracticeInstanceKey ./jenkins/deploy/publish ubuntu@18.205.119.48:/tmp/publish
-ssh -i /home/meena/MyPracticeInstanceKey ubuntu@18.205.119.48  "/tmp/publish"
+scp -i /opt/MyPracticeInstanceKey /tmp/.auth ubuntu@18.205.119.48:/tmp/.auth
+scp -i /opt/MyPracticeInstanceKey ./jenkins/deploy/publish ubuntu@18.205.119.48:/tmp/publish
+ssh -i /opt/MyPracticeInstanceKey ubuntu@18.205.119.48  "/tmp/publish"
